d6c5fd2dea22f11541102e2dd42c7470a554ea02
Denys Vitali <denys@denv.it>
2021-12-06 01:47:54 +0100

    wip: day05 in rust

--- rust/day05/main.rs
+++ rust/day05/main.rs
@@ -1,163 +1,154 @@
 use std::{env, fs};
-use std::fmt::{Debug, Display, Formatter};
+use std::collections::HashMap;
+use std::fmt::{Debug};
 
-mod test;
-
-#[derive(Debug, Clone)]
-struct Board {
-    won: bool,
-    content: Vec<Vec<(i32, bool)>>,
+#[derive(Debug)]
+struct Line {
+    from: (i32, i32),
+    to: (i32, i32),
 }
 
-impl Display for Board {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        write!(f, "Board\n")?;
-        write!(f, "{}", self.content
-            .iter()
-            .map(|r|
-                r.iter().map(
-                    |x|
-                        if !x.1 {
-                            format!("{:^3}", x.0)
-                        } else {
-                            format!("{:^3}", "X")
-                        }
-                )
-                    .collect::<Vec<_>>()
-                    .join(" ")
-            )
-            .collect::<Vec<_>>()
-            .join("\n")
-        )
-    }
+struct IntersectionMap {
+    content: HashMap<i32, HashMap<i32, i32>> // Y,X, Count
 }
 
-impl Board {
-    fn mark(&mut self, number: i32) {
-        for r in &mut self.content {
-            for mut c in r {
-                if c.0 == number {
-                    c.1 = true
+impl IntersectionMap {
+    pub fn print(&self){
+        for i in 0..self.content.len() {
+            let has_i = self.content.contains_key(&(i as i32));
+            for j in 0..self.content.iter().next().unwrap().1.len()+1 {
+                if !has_i  {
+                    print!(".");
+                    continue
                 }
+                let has_j = self.content.get(&(i as i32)).unwrap().contains_key(&(j as i32));
+                if !has_j {
+                    print!(".");
+                    continue
+                }
+                let v = self.content
+                    .get(&(i as i32))
+                    .unwrap()
+                    .get(&(j as i32))
+                    .unwrap_or(&0);
+                print!("{}", v)
             }
+            println!()
         }
     }
 
-    fn won(&mut self) -> bool {
-        if self.won {
-            return true;
-        }
-        // Check Rows
-        for i in &mut self.content {
-            // Count marked in this row
-            if i.iter().filter(|x| !(x.1)).count() == 0 {
-                self.won = true;
-                return true;
-            }
-        }
-
-        // Check Columns
-        for j in 0..self.content[0].len() {
-            let mut unmarked = 0;
-            for (i, _) in self.content.iter().enumerate() {
-                if !self.content[i][j].1 {
-                    unmarked += 1;
-                }
-            }
-            if unmarked == 0 {
-                self.won = true;
-                return true;
-            }
-        }
-        return false;
-    }
-
-    fn score(&self) -> i32 {
+    pub fn overlaps(&self) -> i32 {
         self.content
             .iter()
-            .map(|x| x.iter()
-                .filter(|y| !y.1)
-                .map(|y| y.0).sum::<i32>()
+            .map(|x| x.1)
+            .map(
+                |m|
+                    m.iter()
+                        .filter(|x|*(x.1) >= 2)
             )
-            .sum::<i32>()
+            .count() as i32
     }
 }
 
-fn parse_line(number_line: &str) -> Vec<(i32, bool)> {
-    number_line
-        .trim()
-        .split(" ")
-        .filter(|x| x != &"")
-        .map(|x| (x.parse::<i32>().unwrap(), false))
-        .collect::<Vec<_>>()
-}
+impl Line {
+    pub fn new(from: (i32, i32), to: (i32, i32)) -> Self {
+        return Line{
+            from,
+            to,
+        }
+    }
 
-fn parse_board(board_lines: &str) -> Board {
-    Board {
-        content: board_lines
-            .split("\n")
-            .filter(|x| x != &"")
-            .map(parse_line)
-            .collect::<Vec<_>>(),
-        won: false,
+    pub fn draw(&self, map: &mut IntersectionMap){
+        // Y
+        let (mut from_y, mut to_y) = (self.from.1, self.to.1);
+        if from_y > to_y {
+            let tmp = from_y;
+            from_y = to_y;
+            to_y = tmp;
+        }
+
+        // X
+        let (mut from_x, mut to_x) = (self.from.0, self.to.0);
+        if from_x > to_x {
+            let tmp = from_x;
+            from_x = to_x;
+            to_x = tmp;
+        }
+
+        for i in from_y..to_y+1 {
+            if !map.content.contains_key(&i) {
+                map.content.insert(i, HashMap::new());
+            }
+            let map_i = map.content.get_mut(&i).unwrap();
+            for j in from_x..to_x+1 {
+                if !map_i.contains_key(&j) {
+                    map_i.insert(j, 0);
+                }
+                let prev_value = map_i.get(&j).unwrap().clone();
+                map_i.insert(j, prev_value + 1);
+            }
+        }
+    }
+
+    pub fn is_horizontal(&self) -> bool {
+        // Y doesn't change
+        if self.from.1 == self.to.1 {
+            return true
+        }
+        return false
+    }
+
+    pub fn is_vertical(&self) -> bool {
+        // X doesn't change
+        if self.from.0 == self.to.0 {
+            return true
+        }
+        return false
     }
 }
 
-fn get_draw_list(content: &str) -> Vec<i32> {
-    content
-        .split("\n")
-        .next()
-        .unwrap()
+fn parse_coord(input: &str) -> (i32, i32) {
+    let v = input
         .split(",")
-        .map(|x| x.parse::<i32>().unwrap())
-        .collect()
+        .map(|x|x.parse::<i32>().unwrap_or(-1))
+        .collect::<Vec<_>>();
+    return (v[0], v[1]);
 }
 
-fn get_boards(content: &str) -> Vec<Board> {
-    content.split("\n")
-        .skip(2)
-        .collect::<Vec<_>>()
-        .join("\n")
-        .split("\n\n")
-        .map(parse_board)
-        .collect::<Vec<Board>>()
+fn parse_line(input: &str) -> Line {
+    let v = input
+        .split(" -> ")
+        .map(parse_coord)
+        .collect::<Vec<_>>();
+    Line::new(v[0], v[1])
 }
+
+fn parse_lines(input: &str) -> Vec<Line> {
+    input
+        .split("\n")
+        .map(parse_line)
+        .collect::<Vec<_>>()
+}
+
 
 fn part_one(input_file: &str) -> i32 {
     let content = fs::read_to_string(input_file).unwrap();
-    let draw_list: Vec<_> = get_draw_list(&content);
-    let mut boards = get_boards(&content);
+    let lines = parse_lines(&content);
 
-    for d in draw_list {
-        for b in &mut boards {
-            b.mark(d);
-            if b.won() {
-                return d * b.score();
-            }
+    let mut map : IntersectionMap = IntersectionMap{ content: HashMap::new() };
+    for line in lines {
+        if line.is_horizontal() || line.is_vertical() {
+            line.draw(&mut map)
         }
+        println!("{:?}", line)
     }
-    return -1;
+
+    map.print();
+    return map.overlaps();
 }
 
 fn part_two(input_file: &str) -> i32 {
-    let content = fs::read_to_string(input_file).unwrap();
-    let draw_list: Vec<_> = get_draw_list(&content);
-    let mut boards = get_boards(&content);
-
-    for d in draw_list {
-        // Player playing alone?
-        let is_playing_alone = boards.iter().filter(|b|!b.won).count() == 1;
-
-        for b in &mut boards.iter_mut().filter(|b|!b.won) {
-            if b.won {
-                continue
-            }
-            b.mark(d);
-            if b.won() && is_playing_alone {
-                return d * b.score();
-            }
-        }
-    }
+    let _content = fs::read_to_string(input_file).unwrap();
     return -1;
 }
 
