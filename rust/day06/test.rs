4fd8792fde963a49a5563bf99cd8a4ebfdded68a
Denys Vitali <denys@denv.it>
2021-12-08 10:30:04 +0100

    fix: tests for day06 and day07

--- rust/day06/test.rs
+++ rust/day06/test.rs
@@ -9,16 +9,16 @@ mod tests {
 
     #[test]
     fn sample_part_two() {
-        assert_eq!(part_two("input/sample.txt"), 1924)
+        assert_eq!(part_two("input/sample.txt"), 26984457539)
     }
 
     #[test]
     fn input_part_one() {
-        assert_eq!(part_one("input/input.txt"), 25023)
+        assert_eq!(part_one("input/input.txt"), 351092)
     }
 
     #[test]
     fn input_part_two() {
-        assert_eq!(part_two("input/input.txt"), 2634)
+        assert_eq!(part_two("input/input.txt"), 1595330616005)
     }
 }
--- rust/day07/test.rs
+++ rust/day07/test.rs
@@ -4,21 +4,21 @@ mod tests {
 
     #[test]
     fn sample_part_one() {
-        assert_eq!(part_one("input/sample.txt"), 5934)
+        assert_eq!(part_one("input/sample.txt"), 37)
     }
 
     #[test]
     fn sample_part_two() {
-        assert_eq!(part_two("input/sample.txt"), 1924)
+        assert_eq!(part_two("input/sample.txt"), 168)
     }
 
     #[test]
     fn input_part_one() {
-        assert_eq!(part_one("input/input.txt"), 25023)
+        assert_eq!(part_one("input/input.txt"), 356958)
     }
 
     #[test]
     fn input_part_two() {
-        assert_eq!(part_two("input/input.txt"), 2634)
+        assert_eq!(part_two("input/input.txt"), 105461913)
     }
 }
